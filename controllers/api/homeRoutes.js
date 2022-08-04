1c7e5b7b771949ed18a4062f32743f4984ef0373
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-04 04:05:51 -0500

    tweak handlebar home route

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -2,12 +2,12 @@ const router = require ('express').Router();
 const {User, Posts} = require ('../../models');
 
 router.get('/', async (req, res) => {
-    // try {
-    //     res.status(200).render('homepage', {value: 'hello world'})
-    // } catch (err) {
-    //     res.status(400).json(err)
-    // }
-    res.render('homepage', {value:'hello world'})
+    try {
+        res.status(200).render('homepage', {value: 'hello world'})
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require ('express');
 const session = require('express-session');
 const routes = require ('./controllers');
 
-const exphbs = require('express-handlebars');
-
+// const exphbs = require('express-handlebars');
+const {engine} = require('express-handlebars')
 const sequelize = require ('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express ();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({})
+// const hbs = exphbs.screate({})
 
 const sess = {
     secret: 'secret string',
@@ -23,7 +23,7 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars')
 
 app.set('views', './views');
