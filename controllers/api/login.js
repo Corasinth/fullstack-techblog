f2e28ee6ee6d0657874950334811f1109fafd678
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-05 01:06:49 -0500

    add database check for login

--- controllers/api/login.js
+++ controllers/api/login.js
@@ -11,7 +11,25 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     try {
-        res.status(200).json('All good')
+        const userData = await User.findOne({
+            where: {
+                username: req.body.username
+            }
+        })
+        if (!userData) {
+            res.status(400).json({message: 'Incorrect email or password.'})
+            return;    
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({message: 'Incorrect email or password.'});
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.json({user:userData, message: 'Successfully logged in.'})
+        });
     } catch (err) {
         res.status(400).json(err);
     }
