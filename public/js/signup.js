a6aa077fa17c1056db70420cdfcd6501f75da93f
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-05 00:59:42 -0500

    remove email from signup procedure

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,19 +2,17 @@ const signupForm = document.querySelector('.signup-form');
 
 async function signup(event) {
     event.preventDefault()
-    const email = document.querySelector('#AddEmail').value.trim();
     const username = document.querySelector('#AddUsername').value.trim();
     const password = document.querySelector('#CreatePassword').value.trim();
     const password2 = document.querySelector('#ConfirmPassword').value.trim();
-    console.log (email, username, password, password2)
     if (password !== password2) {
         alert('passwords do not match');
         return;
     }
-    if (email && password && username) {
+    if (password && username) {
         const response = await fetch('/api/login/signup', {
             method: 'POST',
-            body: JSON.stringify({ email, username, password }),
+            body: JSON.stringify({username, password }),
             headers: { 'Content-Type': 'aplication/json' },
         })
         if (response.ok) {
