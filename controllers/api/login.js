7cc63d3bcf56a881c051edfb730985f9195c24a0
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-05 01:12:56 -0500

    create route to create user

--- controllers/api/login.js
+++ controllers/api/login.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {User} = require('../../models/user');
+const {User} = require('../../models');
 
 router.get('/', (req, res) => {
     try {
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
     }
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const userData = await User.findOne({
             where: {
@@ -43,9 +43,10 @@ router.get('/signup', (req, res) => {
     }
 })
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     try {
-        res.status(200).json('allgood')
+        const userData = await User.create(req.body);
+        res.status(200).json(userData)
     } catch (err) {
         res.status(400).json(err);
     }
