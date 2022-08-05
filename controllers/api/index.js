51c1ddf31e3f754f91ae3fd4bb1125243c9052ca
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-04 23:03:20 -0500

    update index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const loginRoutes = require('./login.js');
 
-// const blogpostRoutes = require ('');
-
 router.use('/login', loginRoutes);
-// router.use('/blogposts', blogpostRoutes);
 
 module.exports = router;
